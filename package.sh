363fc43433568909012f7e6955057c74e695d8f1
James Gordon <gordon.je@gmail.com>
2021-03-08 14:43:02 -0600

    add dependencies for rendering web pages

--- package.sh
+++ package.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # prepare a source code package
 
-# pip install --upgrade --target package/ -r requirements.txt
-# cd package
-# zip -r9 ${OLDPWD}/package.zip .
-# cd ${OLDPWD}
+# add dependencies
+pip install --upgrade --target package/ -r requirements.txt
+cd package
+zip -r9 ${OLDPWD}/package.zip .
+rm -rf package
+cd ${OLDPWD}
 
+# add source code
+zip -g -r package.zip templates/*
 zip -g -r package.zip function.py
 zip -g -r package.zip s3.py
-
-# rm -rf package
